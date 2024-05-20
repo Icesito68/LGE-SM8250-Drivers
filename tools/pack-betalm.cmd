b5f963b1c3589c0b4465b7e396a95cb05c6691cb
remtrik <remtrik@mail.ru>
2024-05-20 21:57:59 +0300

    Update pack-betalm.cmd

--- tools/pack-betalm.cmd
+++ tools/pack-betalm.cmd
@@ -1,35 +1,46 @@
 @echo off
 
+mkdir ..\..\LGE-Drivers-Release
+del ..\..\LGE-Drivers-Release\LGE-Drivers-Desktop-betalm.zip
 
-      SET HOUR=%time:~0,2%
-      SET dtStamp9=%date:~-2%%date:~4,2%%date:~7,2%-0%time:~1,1%%time:~3,2%
-      SET dtStamp24=%date:~-2%%date:~4,2%%date:~7,2%-%time:~0,2%%time:~3,2%
+echo @echo off > ..\OnlineUpdater.cmd
+echo ^(NET FILE^|^|^(powershell -command Start-Process '%%0' -Verb runAs -ArgumentList '%%* '^&EXIT /B^)^)^>NUL 2^>^&1 >> ..\OnlineUpdater.cmd
+echo pushd "%%~dp0" ^&^& cd %%~dp0 >> ..\OnlineUpdater.cmd
+echo .\tools\DriverUpdater\%%PROCESSOR_ARCHITECTURE%%\DriverUpdater.exe -r . -d .\definitions\Desktop\ARM64\Internal\betalm.xml >> ..\OnlineUpdater.cmd
+echo pause >> ..\OnlineUpdater.cmd
 
-      if "%HOUR:~0,1%" == " " (SET dtStamp=%dtStamp9%) else (SET dtStamp=%dtStamp24%)
+echo @echo off > ..\OfflineUpdater.cmd
+echo ^(NET FILE^|^|^(powershell -command Start-Process '%%0' -Verb runAs -ArgumentList '%%* '^&EXIT /B^)^)^>NUL 2^>^&1 >> ..\OfflineUpdater.cmd
+echo pushd "%%~dp0" ^&^& cd %%~dp0 >> ..\OfflineUpdater.cmd
+echo set /P DrivePath=Enter Drive letter ^^^(with the colon!^^^) of the connected device in mass storage mode ^^^(e.g. D:^^^): >> ..\OfflineUpdater.cmd
+echo .\tools\DriverUpdater\%%PROCESSOR_ARCHITECTURE%%\DriverUpdater.exe -r . -d .\definitions\Desktop\ARM64\Internal\betalm.xml -p %%DrivePath%% >> ..\OfflineUpdater.cmd
+echo pause >> ..\OfflineUpdater.cmd
 
+echo apps\IPA > filelist_betalm.txt
+echo CODE_OF_CONDUCT.md >> filelist_betalm.txt
+echo components\ANYSOC\Changelog >> filelist_betalm.txt
+echo components\ANYSOC\Support\Desktop\SUPPORT.DESKTOP.BASE >> filelist_betalm.txt
+echo components\ANYSOC\Support\Desktop\SUPPORT.DESKTOP.MOBILE_BRIDGE >> filelist_betalm.txt
+echo components\ANYSOC\Support\Desktop\SUPPORT.DESKTOP.MOBILE_COMPONENTS >> filelist_betalm.txt
+echo components\ANYSOC\Support\Desktop\SUPPORT.DESKTOP.MOBILE_RIL >> filelist_betalm.txt
+echo components\ANYSOC\Support\Desktop\SUPPORT.DESKTOP.MOBILE_RIL_EXTRAS >> filelist_betalm.txt
+echo components\QC8150\Device\DEVICE.SOC_QC8150.betalm >> filelist_betalm.txt
+echo components\QC8150\Device\DEVICE.SOC_QC8150.betalm_MINIMAL >> filelist_betalm.txt
+echo components\QC8150\Graphics\GRAPHICS.SOC_QC8150.betalm_DESKTOP >> filelist_betalm.txt
+echo components\QC8150\Platform\PLATFORM.SOC_QC8150.BASE >> filelist_betalm.txt
+echo components\QC8150\Platform\PLATFORM.SOC_QC8150.BASE_MINIMAL >> filelist_betalm.txt
+echo definitions\Desktop\ARM64\Internal\betalm.xml >> filelist_betalm.txt
+echo tools\DriverUpdater >> filelist_betalm.txt
+echo LICENSE.md >> filelist_betalm.txt
+echo OfflineUpdater.cmd >> filelist_betalm.txt
+echo OnlineUpdater.cmd >> filelist_betalm.txt
+echo README.md >> filelist_betalm.txt
 
-title [220X.%dtStamp%.prerelease] [Build preparation] [Packing Binaries]
-REM rmdir /Q /S ..\..\betalm-Drivers-Release
-REM mkdir ..\..\betalm-Drivers-Release
+cd ..
+"%ProgramFiles%\7-Zip\7z.exe" a -tzip ..\LGE-Drivers-Release\LGE-Drivers-Desktop-betalm.zip @tools\filelist_betalm.txt -scsWIN
+"%ProgramFiles%\7-zip\7z.exe" a -t7z ..\LGE-Drivers-Release\LGE-Drivers-Desktop-betalm.7z @tools\filelist_betalm.txt -scsWIN
+cd tools
 
-mkdir betalm-Drivers-Full
-mkdir betalm-Drivers-Full\components
-mkdir betalm-Drivers-Full\definitions
-
-xcopy /cheriky ..\components\QC8150\Device\DEVICE.SOC_QC8150.BETALM betalm-Drivers-Full\components\QC8150\Device\DEVICE.SOC_QC8150.BETALM
-xcopy /cheriky ..\components\QC8150\Device\DEVICE.SOC_QC8150.BETALM_MINIMAL betalm-Drivers-Full\components\QC8150\Device\DEVICE.SOC_QC8150.BETALM_MINIMAL
-
-xcopy /cheriky ..\components\QC8150\Platform betalm-Drivers-Full\components\QC8150\Platform
-
-xcopy /cheriky ..\components\QC8150\Graphics\GRAPHICS.SOC_QC8150.BETALM_DESKTOP betalm-Drivers-Full\components\QC8150\Graphics\GRAPHICS.SOC_QC8150.BETALM_DESKTOP
-
-xcopy /cheriky ..\components\ANYSOC betalm-Drivers-Full\components\ANYSOC
-
-xcopy /cheriky ..\definitions\Desktop betalm-Drivers-Full\definitions\Desktop
-
-"%ProgramFiles%\7-zip\7z.exe" a -tzip -r ..\..\betalm-Drivers-Release\betalm-Drivers-Full.zip betalm-Drivers-Full\*
-
-REM move betalm-Drivers-Full\components\QC8150 ..\components\QC8150
-REM move betalm-Drivers-Full\definitions\Desktop ..\definitions
-
-rmdir /Q /S betalm-Drivers-Full
+del ..\OfflineUpdater.cmd
+del ..\OnlineUpdater.cmd
+del filelist_betalm.txt
